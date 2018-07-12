1e301c980380bed18600a80665f30c29559ccca3
Ramona M <r1mari@yahoo.com>
2018-07-12 12:25:42 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,6 +4,7 @@ class Hash
     #solution ==> use of optional argument (*arguments)=> can call method with any or no args
     animals= [ ]
     self.each {|animal, town|
+    #The keyword self in Ruby gives you access to the current object – the object that is receiving the current message.
     if arguments.include?(town)
       animals << animal
     end 
