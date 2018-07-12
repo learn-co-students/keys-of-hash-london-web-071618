475c561a052af5e0a5ace22dd38556686a7b36b9
Ramona M <r1mari@yahoo.com>
2018-07-12 12:24:58 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,14 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    #solution ==> use of optional argument (*arguments)=> can call method with any or no args
     animals= [ ]
-    
+    self.each {|animal, town|
+    if arguments.include?(town)
+      animals << animal
+    end 
+      
+    }
+    return animals
   end
 end
